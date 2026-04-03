d91f49bc882b26248395b115d47ae46ace9323ed
Dhruv Gupta <itsdhruvg@gmail.com>
2026-04-03 15:11:13 +0530

    refactor: update package names and identifiers to align with Kynetix branding

--- calorie_tracker_ui/android/app/build.gradle.kts
+++ calorie_tracker_ui/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.calorietracker.calorie_tracker_ui"
+    namespace = "com.kynetix.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.calorietracker.calorie_tracker_ui"
+        applicationId = "com.kynetix.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
--- calorie_tracker_ui/test/widget_test.dart
+++ calorie_tracker_ui/test/widget_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:calorie_tracker_ui/main.dart';
+import 'package:kynetix/main.dart';
 
 void main() {
   testWidgets('App boots without crashing', (WidgetTester tester) async {
-    await tester.pumpWidget(const CalorieTrackerApp());
+    await tester.pumpWidget(const KynetixApp());
     // Onboarding screen should appear on first launch.
     expect(find.byType(MaterialApp), findsOneWidget);
   });
